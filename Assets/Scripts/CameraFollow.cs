a71aafa472b18fef173762c0362dcd2cb503df19
Kyle Rebstock <kjr247@gmail.com>
2020-05-23 19:17:15 -0400

    initial commit Created a planet with a person and gravity. Added Camera follow.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 cameraOffset;
+    public float cameraMoveSpeed = 0.0f;
+    public float cameraMovementSensitivity = 0.2f;
+    public float timer = 0.0f;
+
+    // Update is called once per frame
+    void LastUpdate()
+    {
+        UpdateCamera();
+    }
+
+    private void UpdateCamera()
+    {
+        timer = timer + Time.deltaTime;
+        if (timer >= cameraMovementSensitivity)
+        {
+            timer = 0.0f;
+            Vector3 lookAtTargetPostition = new Vector3(
+                player.transform.position.x,
+                this.transform.position.y,
+                player.transform.position.z
+            );
+            Vector3 followTargetPostition = player.transform.position + cameraOffset;
+            this.transform.LookAt(followTargetPostition);
+        }
+    }
+}
