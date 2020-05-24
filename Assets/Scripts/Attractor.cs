1449830de2bcc3302467fa5e97ca90074fed86d8
Kyle Rebstock <kjr247@gmail.com>
2020-05-23 22:16:35 -0400

    bigger planet, and first person walker

--- Assets/Scripts/Attractor.cs
+++ Assets/Scripts/Attractor.cs
@@ -1,51 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class Attractor : MonoBehaviour {
+// public class Attractor : MonoBehaviour {
 
-	const float G = 667.4f;
+// 	const float G = 667.4f;
 
-	public static List<Attractor> Attractors;
+// 	public static List<Attractor> Attractors;
 
-	public Rigidbody rb;
+// 	public Rigidbody rb;
 
-	void FixedUpdate ()
-	{
-		foreach (Attractor attractor in Attractors)
-		{
-			if (attractor != this)
-				Attract(attractor);
-		}
-	}
+// 	void FixedUpdate ()
+// 	{
+// 		foreach (Attractor attractor in Attractors)
+// 		{
+// 			if (attractor != this)
+// 				Attract(attractor);
+// 		}
+// 	}
 
-	void OnEnable ()
-	{
-		if (Attractors == null)
-			Attractors = new List<Attractor>();
+// 	void OnEnable ()
+// 	{
+// 		if (Attractors == null)
+// 			Attractors = new List<Attractor>();
 
-		Attractors.Add(this);
-	}
+// 		Attractors.Add(this);
+// 	}
 
-	void OnDisable ()
-	{
-		Attractors.Remove(this);
-	}
+// 	void OnDisable ()
+// 	{
+// 		Attractors.Remove(this);
+// 	}
 
-	void Attract (Attractor objToAttract)
-	{
-		Rigidbody rbToAttract = objToAttract.rb;
+// 	void Attract (Attractor objToAttract)
+// 	{
+// 		Rigidbody rbToAttract = objToAttract.rb;
 
-		Vector3 direction = rb.position - rbToAttract.position;
-		float distance = direction.magnitude;
+// 		Vector3 direction = rb.position - rbToAttract.position;
+// 		float distance = direction.magnitude;
 
-		if (distance == 0f)
-			return;
+// 		if (distance == 0f)
+// 			return;
 
-		float forceMagnitude = G * (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
-		Vector3 force = direction.normalized * forceMagnitude;
+// 		float forceMagnitude = G * (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
+// 		Vector3 force = direction.normalized * forceMagnitude;
 
-		rbToAttract.AddForce(force);
-	}
+// 		rbToAttract.AddForce(force);
+// 	}
 
-}
+// }
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,53 +1,53 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿// using UnityEngine;
+// using System.Collections;
 
-// This script moves the character controller forward
-// and sideways based on the arrow keys.
-// It also jumps when pressing space.
-// Make sure to attach a character controller to the same game object.
-// It is recommended that you make only one call to Move or SimpleMove per frame.
+// // This script moves the character controller forward
+// // and sideways based on the arrow keys.
+// // It also jumps when pressing space.
+// // Make sure to attach a character controller to the same game object.
+// // It is recommended that you make only one call to Move or SimpleMove per frame.
 
-public class PlayerMove : MonoBehaviour
-{
-    CharacterController characterController;
-    // Gravity gravity;
+// public class PlayerMove : MonoBehaviour
+// {
+//     CharacterController characterController;
+//     // Gravity gravity;
 
-    public bool isGrounded = false;
-    public float speed = 6.0f;
-    public float jumpSpeed = 8.0f;
-    private float gravity = 20.0f;
+//     public bool isGrounded = false;
+//     public float speed = 6.0f;
+//     public float jumpSpeed = 8.0f;
+//     private float gravity = 20.0f;
 
-    private Vector3 moveDirection = Vector3.zero;
+//     private Vector3 moveDirection = Vector3.zero;
 
-    void Awake()
-    {
-        characterController = GetComponent<CharacterController>();
-        // gravity = GetComponent<Gravity>();
-        characterController.enabled = true;
-    }
+//     void Awake()
+//     {
+//         characterController = GetComponent<CharacterController>();
+//         // gravity = GetComponent<Gravity>();
+//         characterController.enabled = true;
+//     }
 
-    void FixedUpdate()
-    {
+//     void FixedUpdate()
+//     {
 
-            // We are grounded, so recalculate
-            // move direction directly from axes
+//             // We are grounded, so recalculate
+//             // move direction directly from axes
 
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-            moveDirection *= speed;
+//             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+//             moveDirection *= speed;
 
-            // if (Input.GetButton("Jump"))
-            // {
-            //     Vector3 jumpVector = new Vector3(gravity.force)
-            //     gravity.rb.AddForce();
-            // }
+//             // if (Input.GetButton("Jump"))
+//             // {
+//             //     Vector3 jumpVector = new Vector3(gravity.force)
+//             //     gravity.rb.AddForce();
+//             // }
         
 
-        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
-        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
-        // as an acceleration (ms^-2)
-        // moveDirection.y -= Time.deltaTime;
+//         // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+//         // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+//         // as an acceleration (ms^-2)
+//         // moveDirection.y -= Time.deltaTime;
 
-        // Move the controller
-        characterController.Move(moveDirection * Time.deltaTime);
-    }
-}
+//         // Move the controller
+//         characterController.Move(moveDirection * Time.deltaTime);
+//     }
+// }
