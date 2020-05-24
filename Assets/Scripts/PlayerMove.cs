9d1fd4d321ce66811f1ae9f7384d46bb4b7227ba
Kyle Rebstock <kjr247@gmail.com>
2020-05-23 22:04:08 -0400

    missed a script for playermove, not that it works with a round world or anything. bleh

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -10,45 +10,42 @@ using System.Collections;
 public class PlayerMove : MonoBehaviour
 {
     CharacterController characterController;
+    // Gravity gravity;
 
     public bool isGrounded = false;
     public float speed = 6.0f;
     public float jumpSpeed = 8.0f;
-    public float gravity = 20.0f;
+    private float gravity = 20.0f;
 
     private Vector3 moveDirection = Vector3.zero;
 
-    void OnEnable(){ //or Awake() or Start()
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        // gravity = GetComponent<Gravity>();
         characterController.enabled = true;
     }
 
-    void Start()
+    void FixedUpdate()
     {
-        characterController = GetComponent<CharacterController>();
-    }
 
-    void Update()
-    {
-        isGrounded = characterController.isGrounded;
-        Debug.Log("Text: " + isGrounded);
-        if (characterController.isGrounded)
-        {
             // We are grounded, so recalculate
             // move direction directly from axes
 
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
             moveDirection *= speed;
 
-            if (Input.GetButton("Jump"))
-            {
-                moveDirection.y = jumpSpeed;
-            }
-        }
+            // if (Input.GetButton("Jump"))
+            // {
+            //     Vector3 jumpVector = new Vector3(gravity.force)
+            //     gravity.rb.AddForce();
+            // }
+        
 
         // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
         // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
         // as an acceleration (ms^-2)
-        moveDirection.y -= gravity * Time.deltaTime;
+        // moveDirection.y -= Time.deltaTime;
 
         // Move the controller
         characterController.Move(moveDirection * Time.deltaTime);
