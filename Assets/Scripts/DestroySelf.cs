c7415c802a14f8835a6b31cedc7480b083c5bf4c
Kyle Rebstock <kjr247@gmail.com>
2020-05-25 23:13:29 -0400

    added the instantiation of a meteor on start this doesn't destroy or clean itself up after colliding with the planet yet. That's the next step here.

---
+++ Assets/Scripts/DestroySelf.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySelf : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other) {
+        Debug.Log("collision");
+        if(other.collider.tag == "Planet") {
+            Destroy(gameObject);
+        }
+    }
+}
