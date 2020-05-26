4cc346f946ab361d37ff5cdfb07cbd246a205de6
Kyle Rebstock <kjr247@gmail.com>
2020-05-25 21:07:59 -0400

    - :zap: added a minimap - :zap: fixed gravity and jumping - :zap: adjusted cameras

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -13,58 +13,78 @@ public class FirstPersonController : MonoBehaviour {
 	public LayerMask groundedMask;
 	
 	// System vars
-	bool grounded;
+	public bool isGrounded;
 	Vector3 moveAmount;
 	Vector3 smoothMoveVelocity;
 	float verticalLookRotation;
 	Transform cameraTransform;
 	Rigidbody rigidbody;
-	
-	
+
+
 	void Awake() {
 		// Cursor.lockState = CursorLockMode.Locked;
 		Cursor.visible = false;
 		cameraTransform = Camera.main.transform;
 		rigidbody = GetComponent<Rigidbody> ();
 	}
-	
-	void Update() {
-		
-		// Look rotation:
-		transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * mouseSensitivityX);
-		verticalLookRotation += Input.GetAxis("Mouse Y") * mouseSensitivityY;
-		verticalLookRotation = Mathf.Clamp(verticalLookRotation,-60,60);
-		cameraTransform.localEulerAngles = Vector3.left * verticalLookRotation;
-		
-		// Calculate movement:
-		float inputX = Input.GetAxisRaw("Horizontal");
-		float inputY = Input.GetAxisRaw("Vertical");
-		
-		Vector3 moveDir = new Vector3(inputX,0, inputY).normalized;
-		Vector3 targetMoveAmount = moveDir * walkSpeed;
-		moveAmount = Vector3.SmoothDamp(moveAmount,targetMoveAmount,ref smoothMoveVelocity,.15f);
-		
-		// Jump
-		if (Input.GetButtonDown("Jump")) {
-			if (grounded) {
-				rigidbody.AddForce(transform.up * jumpForce);
-			}
-		}
-		
-		// Grounded check
-		Ray ray = new Ray(transform.position, -transform.up);
-		RaycastHit hit;
-		
-		if (Physics.Raycast(ray, out hit, 1 + .1f, groundedMask)) {
-			grounded = true;
-		}
-		else {
-			grounded = false;
-		}
-		
-	}
-	
-	void FixedUpdate() {
+
+	void Update()
+    {
+        // Look rotation:
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * mouseSensitivityX);
+        verticalLookRotation += Input.GetAxis("Mouse Y") * mouseSensitivityY;
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation,-60,60);
+        cameraTransform.localEulerAngles = Vector3.left * verticalLookRotation;
+
+        // Calculate movement:
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveDir = new Vector3(inputX, 0, inputY).normalized;
+        Vector3 targetMoveAmount = moveDir * walkSpeed;
+        moveAmount = Vector3.SmoothDamp(moveAmount, targetMoveAmount, ref smoothMoveVelocity, .15f);
+
+        HandleJump();
+    }
+
+    private void HandleJump()
+    {
+        GetAndSetIsGrounded();
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        {
+            rigidbody.AddForce(transform.up * jumpForce);
+
+            // Debug.Log("Space key was pressed.");
+            // Debug.Log("isGrounded:");
+            // Debug.Log(isGrounded);
+        }
+
+        // if (Input.GetKeyUp(KeyCode.Space))
+        // {
+        //     Debug.Log("Space key was released.");
+        //     Debug.Log("isGrounded:");
+        //     Debug.Log(isGrounded);
+        // }
+    }
+
+    private void GetAndSetIsGrounded()
+    {
+        Ray ray = new Ray(transform.position, -transform.up);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 5f, groundedMask))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    void FixedUpdate() {
+
 		// Apply movement to rigidbody
 		Vector3 localMove = transform.TransformDirection(moveAmount) * Time.fixedDeltaTime;
 		rigidbody.MovePosition(rigidbody.position + localMove);
